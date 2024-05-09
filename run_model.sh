7df9192dbea84b7a5c5f742bac0638c28eb4cfce
Avisha Das <dasavisha@users.noreply.github.com>
2024-05-09 15:21:04 -0700

    Delete run_model.sh

--- run_model.sh
+++
@@ -1,7 +0,0 @@
-python main.py \
---model_name "mistralai/Mistral-7B-Instruct-v0.2" \
---dataset_name "./data_file.csv" \
---output_file "./generated_responses_MISTRAL_ZS.csv" \
---type "ZS"
-
-python keyterm_extract.py generated_responses_MISTRAL_ZS.csv extracted_keyterms_MISTRAL_ZS.csv
